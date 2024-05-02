8b926ce4d7d5c4e602982173b11d9db356a6eaf5
drussell1974 <dave@daverussell.co.uk>
2024-05-02 16:15:15 +0100

    #6 handle errors processing materials for job card. close #7 font resizes to fit.

--- src/mattress_build/app/Mattress_Builds.py
+++ src/mattress_build/app/Mattress_Builds.py
@@ -20,8 +20,8 @@ class JobCardDocument:
     """ class to handle the job card document """
     
     def __init__(self, source_file, source_data):
-
-        self.doc = fitz.open(source_file)
+        self.source_file = source_file
+        self.doc = None
         self.source_data = source_data
         self.build_data = None
         self.pages = []
@@ -66,17 +66,25 @@ class JobCardDocument:
 
     ''' public methods '''
 
-    def generate_new_label(self, table_name="Builds"):
+    def generate_new_label(self, table_name="Builds", on_jobcardpage_error=None):
         """ process the full_dataset and append the build information to the PDF """
-
+        
+        self.doc = fitz.open(self.source_file)
+        
+        if self.doc is None:
+            raise FileNotFoundError(f"Document '{self.source_file}' not opened!")
+        
         full_dataset = JobCardDataAccess.get_all(self.source_data, table_name=table_name)
 
         for page_num, page in self._get_doc_pages():
             sku_prefix = self._extract_sku(page)
-            
-            self.build_data = JobCardDataAccess.get_build_data(full_dataset, sku_prefix)
-            
-            if self.build_data is not None:
+
+            # get the build data for the SKU prefix
+            self.build_data = JobCardDataAccess.get_build_data(full_dataset, sku_prefix, on_jobcardpage_error, source_data=self.source_data, page_num=page_num)
+            if self.build_data is None or len(self.build_data) == 0:
+                self.build_data = [f"No build data found for SKU prefix: '{sku_prefix}'"]
+            else:
+                # append the build data to the page
                 self._append_html(page, self.build_data)
                 self.pages.append(page)
 
@@ -112,64 +120,97 @@ class JobCardDataAccess:
 
     
     @classmethod
-    def get_build_data(cls, full_dataset, sku_prefix):
+    def get_build_data(cls, full_dataset, sku_prefix, on_jobcarddataaccess_error=None, **kwargs):
         """ get the build information for the SKU prefix
             return: string with semi-colon seperated values of data """
         ''' ref https://pandas.pydata.org/pandas-docs/version/1.3/user_guide/indexing.html#indexing-lookup '''
-        
         # initialise return value as None
         build_data = []
         
-        # clean the data and add to a dataframe for lookup
-        df_all_rows = pd.DataFrame(full_dataset, columns=[cls.SKU_PREFIX_COL_NAME, cls.SKU_COL_NAME, cls.MATERIALS_COL_NAME, cls.QTY_COL_NAME])
-        
-        # get row for selected sku_prefix
-        df_subset_first_line = df_all_rows[df_all_rows[cls.SKU_PREFIX_COL_NAME].str.contains(sku_prefix, na=False)]
-        
-        # check if this is the first row containing the value 'Material' in the column
-        if df_subset_first_line.values[0][2] != "Material":
-            '''raise error to be caught by the calling function e.g. to be displayed or logged'''
-            raise KeyError("The line does no contain the value 'Material' column.", "row:", df_subset_first_line.values[0], "expected value='Material', actual value='", df_subset_first_line.values[0][2], "'")    
-        
-        else:
-            '''get the build data from rows below the selected line (ensure fillna has been applied to the dataframe)''' 
-            df_all_below_first_line = df_all_rows[df_all_rows.index > df_subset_first_line.index[0]]
-            # get materials and qty columns
-            df_materials_rows = pd.DataFrame(df_all_below_first_line, columns=[cls.MATERIALS_COL_NAME, cls.QTY_COL_NAME])
-            # find first empty row
-            df_materials_rows = df_materials_rows.fillna('')
+        try:
+            # clean the data and add to a dataframe for lookup
+            df_all_rows = pd.DataFrame(full_dataset, columns=[cls.SKU_PREFIX_COL_NAME, cls.SKU_COL_NAME, cls.MATERIALS_COL_NAME, cls.QTY_COL_NAME])
             
-            # get the material value for the selected row then but check the cls.SKU_PREFIX_COL_NAME column is not a sku
-            all_build_data = df_materials_rows[cls.MATERIALS_COL_NAME] #.str.cat(sep=';')
-            # get rows until the first empty row
-            for build_item in all_build_data:
-                # check if the item is empty 
-                if build_item.strip(" ").upper() in cls.EMPTY_MATERIAL_VALUES:
-                    # exit the loop
-                    break
-                else:
-                    # adding item to build_data
-                    build_data.append(build_item)
-        
+            # get row for selected sku_prefix
+            df_subset_first_line = df_all_rows[df_all_rows[cls.SKU_PREFIX_COL_NAME].str.contains(sku_prefix, na=False)]
+            
+            # check if this is the first row containing the value 'Material' in the column
+            if len(df_subset_first_line) == 0 or df_subset_first_line.values[0][2] != "Material":
+                '''raise error to be caught by the calling function e.g. to be displayed or logged'''
+                raise KeyError(f"Row for '{sku_prefix}' does no contain the value 'Material' column.", "row:", df_subset_first_line.values[0], "expected value='Material', actual value='", df_subset_first_line.values[0][2], "'")    
+            
+            else:
+                '''get the build data from rows below the selected line (ensure fillna has been applied to the dataframe)''' 
+                df_all_below_first_line = df_all_rows[df_all_rows.index > df_subset_first_line.index[0]]
+                # get materials and qty columns
+                df_materials_rows = pd.DataFrame(df_all_below_first_line, columns=[cls.MATERIALS_COL_NAME, cls.QTY_COL_NAME])
+                # find first empty row
+                df_materials_rows = df_materials_rows.fillna('')
+                
+                # get the material value for the selected row then but check the cls.SKU_PREFIX_COL_NAME column is not a sku
+                all_build_data = df_materials_rows[cls.MATERIALS_COL_NAME] #.str.cat(sep=';')
+                # get rows until the first empty row
+                for build_item in all_build_data:
+                    # check if the item is empty 
+                    if build_item.strip(" ").upper() in cls.EMPTY_MATERIAL_VALUES:
+                        # final item reached - exit the loop
+                        break
+                    else:
+                        # adding item to build_data
+                        build_data.append(build_item)
+    
+        except KeyError as ky_err:
+            if on_jobcarddataaccess_error is not None:
+                on_jobcarddataaccess_error(ky_err, **kwargs)
+            else:
+                raise ky_err
+        except IndexError as idx_err:
+            if on_jobcarddataaccess_error is not None:
+                on_jobcarddataaccess_error(idx_err, **kwargs)
+            else:
+                raise idx_err
+            
         return build_data
 
 
 # program entry point
+
+# error handling
+
+def handle_general_error(e):
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(f"* FATAL ERROR...")
+    print(f"* An error occurred: {e} ")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+def handle_jobcardpage_error(e, source_data=None, page_num=0):
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(f" Error: {e}.\n\nPAGE {page_num} MAY NOT CONTAIN BUILD DATA.")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(" 1. Check labels before applying to product. ")
+    print(f" 2. Check {source_data}. ")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
 
 if __name__ == '__main__':
 
     try:
         # settings
 
-        source_file = './tests/data/Example job card.pdf'
-        full_dataset_file = './tests/data/Build Example v2.xlsx'
-        output_file = './tests/data/Modified Example job card.pdf'
+        source_file = './tests/data/Original job card TEST.pdf'
+        full_dataset_file = './tests/data/Build Materials Data TEST.xlsx'
+        output_file = './tests/data/Build Materials job card TEST.pdf'
         
         # open the job card template
 
         doc = JobCardDocument(source_file, full_dataset_file)
 
-        doc.generate_new_label("Builds")
+        doc.generate_new_label("Builds", on_jobcardpage_error=handle_jobcardpage_error)
         
         doc.save_and_close(output_file)
 
@@ -178,10 +219,6 @@ if __name__ == '__main__':
         print("**************************************************************")
     
     except Exception as e:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(f"* FATAL ERROR...")
-        print(f"* An error occurred: {e} ")
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        handle_general_error(e)
         raise e
+    
